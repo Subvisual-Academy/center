36a40374a4322245e3765389131a8f122dc04c28
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2023-07-07 14:42:59 +0100

    change to not null on db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_06_174321) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_07_133200) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,13 +33,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_06_174321) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
-    t.string "password_digest"
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.integer "role"
     t.integer "base_office"
     t.string "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
