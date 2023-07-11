ce1182d7f15706dd1659d5929d09530f6a435e58
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2023-07-11 12:14:46 +0100

    code review

--- db/migrate/20230706153437_create_answers.rb
+++ db/migrate/20230706153437_create_answers.rb
@@ -1,7 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration[7.0]
   def change
     create_table :answers do |t|
-      t.string :content
+      t.text :body
       t.references :user, null: false, foreign_key: true
       t.references :question, null: false, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_10_154514) do
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
-    t.text "content"
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
