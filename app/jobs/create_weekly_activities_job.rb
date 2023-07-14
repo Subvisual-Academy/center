8b9f73c7fe0b4bbf4ff6162717fb13e5211c2be3
MFJess <jessica2001mf@gmail.com>
2023-07-14 15:06:39 +0100

    Run standardrb.

--- app/jobs/create_weekly_activities_job.rb
+++ app/jobs/create_weekly_activities_job.rb
@@ -2,24 +2,23 @@ class CreateWeeklyActivitiesJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    user_ids = User.all.order("RANDOM()").pluck(:id) 
+    user_ids = User.all.order("RANDOM()").pluck(:id)
 
-    unused_user_id = User.find_by(not_paired: true)&.id      
+    unused_user_id = User.find_by(not_paired: true)&.id
     return unless unused_user_id
 
-    user_ids = user_ids.unshift(unused_user_id).uniq   
-    
-    users = user_ids.collect { |id| User.find(id) }     
-    
+    user_ids = user_ids.unshift(unused_user_id).uniq
+
+    users = user_ids.collect { |id| User.find(id) }
+
     User.update(unused_user_id, not_paired: false)
 
-    users.each_slice(2) do |user_1, user_2|              
+    users.each_slice(2) do |user_1, user_2|
       if user_2
-        WeeklyActivity.create(user_1: user_1, user_2: user_2)     
+        WeeklyActivity.create(user_1: user_1, user_2: user_2)
       else
         user_1.update(not_paired: true)
       end
     end
   end
 end
-
