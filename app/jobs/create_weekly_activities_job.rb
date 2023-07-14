078771beb8ef5f5e23de6b3d1eb52df74033f194
MFJess <jessica2001mf@gmail.com>
2023-07-14 15:05:02 +0100

    Remove @ from variables. Change activity to activities in job file.

---
+++ app/jobs/create_weekly_activities_job.rb
@@ -0,0 +1,25 @@
+class CreateWeeklyActivitiesJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    user_ids = User.all.order("RANDOM()").pluck(:id) 
+
+    unused_user_id = User.find_by(not_paired: true)&.id      
+    return unless unused_user_id
+
+    user_ids = user_ids.unshift(unused_user_id).uniq   
+    
+    users = user_ids.collect { |id| User.find(id) }     
+    
+    User.update(unused_user_id, not_paired: false)
+
+    users.each_slice(2) do |user_1, user_2|              
+      if user_2
+        WeeklyActivity.create(user_1: user_1, user_2: user_2)     
+      else
+        user_1.update(not_paired: true)
+      end
+    end
+  end
+end
+
