7ead311b28fb993beac35676e37b734667dcf51a
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2023-07-17 13:59:59 +0100

    standardrb

--- app/controllers/questions/answers_controller.rb
+++ app/controllers/questions/answers_controller.rb
@@ -1,10 +1,9 @@
 class Questions::AnswersController < ApplicationController
-    before_action :authorize_request
-  
-    # GET /question/1/answers
-    def index
-      @answers = Answer.all.where(question_id: params["question_id"])
-      render json: @answers
-    end
+  before_action :authorize_request
+
+  # GET /question/1/answers
+  def index
+    @answers = Answer.all.where(question_id: params["question_id"])
+    render json: @answers
   end
-  
+end
