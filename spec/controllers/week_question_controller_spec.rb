9bcd2d72ec6c4763f86009fa95348908da72a790
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2023-07-18 15:15:59 +0100

    RSpec

---
+++ spec/controllers/week_question_controller_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe WeekQuestionController, type: :controller do
+  include ActiveJob::TestHelper
+  describe "GET login" do
+    it "returns a 200" do
+      Question.create(title: "Title", content: "A Question")
+      weekly_question = CreateWeeklyQuestionJob.perform_now
+      get :show
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body["question_id"] == weekly_question.question.id)
+    end
+  end
+end
