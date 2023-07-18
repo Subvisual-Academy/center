cdab140963f8080fef3f9d31326e9264ce873385
MFJess <jessica2001mf@gmail.com>
2023-07-18 12:34:54 +0100

    Change user migration files.

--- db/migrate/20230714134936_rename_column_left.rb
+++
@@ -1,5 +0,0 @@
-class RenameColumnLeft < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :users, :left, :not_paired
-  end
-end
---
+++ db/migrate/20230718112930_add_not_paired_to_users.rb
@@ -0,0 +1,5 @@
+class AddNotPairedToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :not_paired, :boolean, :default => false
+  end
+end
