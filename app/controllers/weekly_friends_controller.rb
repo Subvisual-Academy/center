ea138480ebc9e37346f40d3caed656bf04efc9f2
MFJess <jessica2001mf@gmail.com>
2023-07-21 11:00:47 +0100

    Run standardrb.

--- app/controllers/weekly_friends_controller.rb
+++ app/controllers/weekly_friends_controller.rb
@@ -1,16 +1,14 @@
 class WeeklyFriendsController < ApplicationController
-    def show
-        user = params["id"].to_i
-        weekly_activity = WeeklyActivity.where(user_1_id: user).or(WeeklyActivity.where(user_2_id: user)).first
-        
-        if weekly_activity.nil?
-            render json: user
-        else
-            if user == weekly_activity.user_1_id
-               render json: weekly_activity.user_2_id
-            else
-              render json: weekly_activity.user_1_id
-            end
-        end
+  def show
+    user = params["id"].to_i
+    weekly_activity = WeeklyActivity.where(user_1_id: user).or(WeeklyActivity.where(user_2_id: user)).first
+
+    if weekly_activity.nil?
+      render json: user
+    elsif user == weekly_activity.user_1_id
+      render json: weekly_activity.user_2_id
+    else
+      render json: weekly_activity.user_1_id
     end
+  end
 end
