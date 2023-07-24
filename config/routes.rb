ceca90f0d6992a7d3fac6ae5a87c2350434863f8
MFJess <jessica2001mf@gmail.com>
2023-07-24 11:05:30 +0100

    Fix typo in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :answers, only: [:index], controller: "questions/answers"
   end
   resources :answers
-  resources :weeekly_friends, only: [:show], controller: "weekly_friends"
+  resources :weekly_friends, only: [:show], controller: "weekly_friends"
   post "/auth/login", to: "authentication#login"
   get "/weekly_question", to: "week_question#show"
   get "/*path", to: "application#not_found"
