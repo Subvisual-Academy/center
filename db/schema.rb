a50e385f2c365d6a67ec6a2be8f0ada0747c1dfb
jess <61982705+MFJess@users.noreply.github.com>
2023-07-24 14:23:52 +0100

    Add function to return weekly friend for the frontend. (#20)
    * Add function to return weekly friend for the frontend.

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_21_162145) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.text "body"
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
