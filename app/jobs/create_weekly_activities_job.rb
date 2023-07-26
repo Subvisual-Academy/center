42e1ed19cede0925a66161722025f9495d4fb468
Tiago Passos Rodrigues <63853699+TiagoPR@users.noreply.github.com>
2023-07-26 11:11:29 +0100

    Fix NameError and returning nil (#23)
    Why:
    
    * We have a NameError running the weekly_activity cron job. 
    * If there isn't any User with `not_paired` true the job would never run because of the `return unless unused_user` and we still want it to make the pairs.
    
    This change addresses the need by:
    
    * Changing the name on the `schedule.yml` file.
    * Verifying if there's a `not_paired` user and if there's not we will still run the job.

--- app/jobs/create_weekly_activities_job.rb
+++ app/jobs/create_weekly_activities_job.rb
@@ -3,12 +3,19 @@ class CreateWeeklyActivitiesJob < ApplicationJob
 
   def perform(*args)
     unused_user = User.find_by(not_paired: true)
-    return unless unused_user
 
-    user_ids = User.where(not_paired: false).order("RANDOM()").pluck(:id).unshift(unused_user.id)
+    if unused_user
+      user_ids = User.where(not_paired: false).order("RANDOM()").pluck(:id).unshift(unused_user.id)
+      unused_user.update(not_paired: false)
+    else
+      user_ids = User.order("RANDOM()").pluck(:id)
+    end
+    pair(user_ids)
+  end
 
-    unused_user.update(not_paired: false)
+  private
 
+  def pair(user_ids)
     user_ids.each_slice(2) do |user_1, user_2|
       if user_2
         WeeklyActivity.create(user_1_id: user_1, user_2_id: user_2)
