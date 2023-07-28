7b11e372808c25a51fe1d117b3daf82db1673496
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2023-07-28 11:49:50 +0100

    default picture and remove public and column

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,11 @@ class User < ApplicationRecord
   has_many :answers
   has_many :weekly_activities
   enum :base_office, {braga: 0, coimbra: 1}
+  after_commit :add_default_picture, on: [:create]
+
+  private def add_default_picture
+    unless profile_pic.attached?
+      profile_pic.attach(io: File.open("#{Rails.root}/app/assets/images/profile.png"), filename: "profile.png", content_type: "image/png")
+    end
+  end
 end
