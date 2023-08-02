f3022278006ca04f54a9996b3359722a35964937
MFJess <jessica2001mf@gmail.com>
2023-08-02 19:20:58 +0100

    Correct development environment mailer settings. Fix minor issue in mailer.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,16 +36,6 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.sendgrid.net",
-    port: 587,
-    user_name: "apikey",
-    password: ENV["SENDGRID_API_KEY"],
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
