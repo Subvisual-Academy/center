10d2c8d5be5984696707ceb7d7497f533b3107f8
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2023-08-03 13:05:41 +0100

    fix rspec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,8 +5,10 @@ FactoryBot.define do
 end
 
 FactoryBot.define do
+  company = FactoryBot.create(:company)
   factory :user do
     email { generate(:email) }
     password { "123456" }
+    company_id { company.id }
   end
 end
