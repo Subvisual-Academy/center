b08a8a17f71b5b3e6dd71dda769a7982247a6c8a
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2023-08-07 11:08:55 +0100

    Give the time in which it was the weekly question

--- app/serializers/weekly_question_serializer.rb
+++ app/serializers/weekly_question_serializer.rb
@@ -1,7 +1,13 @@
 class WeeklyQuestionSerializer
   include Alba::Resource
 
-  attributes :id, :week, :created_at, :updated_at
+  attributes :id, :week
+
+  attribute :during do |resource|
+    start_week = resource.week - 7
+    end_week = resource.week - 1
+    "#{start_week.strftime("%d/%m")}-#{end_week.strftime("%d/%m")} "
+  end
 
   one :question
 end
