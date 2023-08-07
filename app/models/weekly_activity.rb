565c5a99236c477bf7fad8a85c609bca62fae9ad
MFJess <jessica2001mf@gmail.com>
2023-08-07 15:48:59 +0100

    Run standardrb.

--- app/models/weekly_activity.rb
+++ app/models/weekly_activity.rb
@@ -2,7 +2,7 @@ class WeeklyActivity < ApplicationRecord
   belongs_to :user_1, class_name: "User"
   belongs_to :user_2, class_name: "User"
 
-  scope :active, -> {where(active: true)}
+  scope :active, -> { where(active: true) }
 
-  scope :includes_user, -> (user_id) { where(user_1_id: user_id).or(where(user_2_id: user_id)) }
+  scope :includes_user, ->(user_id) { where(user_1_id: user_id).or(where(user_2_id: user_id)) }
 end
