290fea5cdb3686bd1a8ec07749cc687248dcf1a7
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2023-08-08 16:46:20 +0100

    requested changes

--- db/migrate/20230802170650_add_company_to_user.rb
+++ db/migrate/20230802170650_add_company_to_user.rb
@@ -2,7 +2,7 @@ class AddCompanyToUser < ActiveRecord::Migration[7.0]
   def change
     add_reference :users, :company, foreign_key: true
 
-    User.update_all(company_id: 1)
+    execute "UPDATE users SET company_id = 1;"
 
     change_column_null :users, :company_id, false
   end
