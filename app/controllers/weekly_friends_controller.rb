bf1a083d22fdb90d47a1bb27b81837cb5d8ac8dd
MFJess <jessica2001mf@gmail.com>
2023-08-09 11:37:45 +0100

    Return error not found in case of not paired.

--- app/controllers/weekly_friends_controller.rb
+++ app/controllers/weekly_friends_controller.rb
@@ -1,16 +1,16 @@
 class WeeklyFriendsController < ApplicationController
   def show
     user = User.find(params["id"])
-    active = WeeklyActivity.active
+    active_activities = WeeklyActivity.active
 
-    if active.where(user_1_id: user.id).exists?
-      render json: active.find_by(user_1_id: user.id).user_2
+    if active_activities.where(user_1: user).exists?
+      render json: active_activities.find_by(user_1: user).user_2
 
-    elsif active.where(user_2_id: user.id).exists?
-      render json: active.find_by(user_2_id: user.id).user_1
+    elsif active_activities.where(user_2: user).exists?
+      render json: active_activities.find_by(user_2: user).user_1
 
     else
-      render json: user
+      render json: {error: "not_found"}, status: 404
     end
   end
 end
