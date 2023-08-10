dda941e78eb85e60d671e5b5a6f0c3ade330d114
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2023-08-10 10:56:22 +0100

    Ensure company exists

--- db/migrate/20230802170650_add_company_to_user.rb
+++ db/migrate/20230802170650_add_company_to_user.rb
@@ -2,6 +2,7 @@ class AddCompanyToUser < ActiveRecord::Migration[7.0]
   def change
     add_reference :users, :company, foreign_key: true
 
+    execute "INSERT INTO companies (name, created_at, updated_at) VALUES ('Subvisual', NOW(), NOW())"
     execute "UPDATE users SET company_id = 1;"
 
     change_column_null :users, :company_id, false
