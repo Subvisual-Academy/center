e2bae240f7549e984d77a945bf01e1ecff9d2a6f
MFJess <jessica2001mf@gmail.com>
2023-08-10 18:01:26 +0100

    Run standardrb.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,5 +1,5 @@
 class CompaniesController < ApplicationController
-  before_action :set_company, only: %i[ show update destroy ]
+  before_action :set_company, only: %i[show update destroy]
 
   # GET /companies
   def index
@@ -39,13 +39,14 @@ class CompaniesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_company
-      @company = Company.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def company_params
-      params.fetch(:company, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_company
+    @company = Company.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def company_params
+    params.fetch(:company, {})
+  end
 end
