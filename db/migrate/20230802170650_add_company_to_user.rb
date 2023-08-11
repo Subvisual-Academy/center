c449c33dd89900186e4c6314b8578493af02fe81
Tiago Passos Rodrigues <63853699+TiagoPR@users.noreply.github.com>
2023-08-11 11:00:32 +0100

    Ensure company exists (#41)
    Why:
    
    * Deploy crashing because of non-existent company
    
    This change addresses the need by:
    
    * Inserting a new company to the table named `Subvisual`

--- db/migrate/20230802170650_add_company_to_user.rb
+++ db/migrate/20230802170650_add_company_to_user.rb
@@ -2,7 +2,8 @@ class AddCompanyToUser < ActiveRecord::Migration[7.0]
   def change
     add_reference :users, :company, foreign_key: true
 
-    execute "UPDATE users SET company_id = 1;"
+    execute "INSERT INTO companies (name, created_at, updated_at) VALUES ('Subvisual', NOW(), NOW())"
+    execute "UPDATE users SET company_id = (select id from companies limit 1);"
 
     change_column_null :users, :company_id, false
   end
