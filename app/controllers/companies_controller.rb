c5595282b11d95125877e5bbef1e6e4f86d35f23
MFJess <jessica2001mf@gmail.com>
2023-08-11 11:29:57 +0100

    Remove create, update and delete methods from companies controller.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -13,31 +13,6 @@ class CompaniesController < ApplicationController
     render json: @company
   end
 
-  # POST /companies
-  def create
-    @company = Company.new(company_params)
-
-    if @company.save
-      render json: @company, status: :created, location: @company
-    else
-      render json: @company.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /companies/1
-  def update
-    if @company.update(company_params)
-      render json: @company
-    else
-      render json: @company.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /companies/1
-  def destroy
-    @company.destroy
-  end
-
   private
 
   # Use callbacks to share common setup or constraints between actions.
