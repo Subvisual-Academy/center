c1a625f31b9031a95fb9f4d61835f357c54c5dff
jess <61982705+MFJess@users.noreply.github.com>
2023-08-16 17:00:15 +0100

    Update user serializer. (#44)
    * Add company name to user serializer
    
    * Add role, office and discord to serializer.
    
    * Correct index method in users controller
    
    * Add profile pic to user serializer.
    
    * Fix typo

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -7,4 +7,9 @@ class UserSerializer
   attribute :profile_pic do |resource|
     resource.profile_pic.url
   end
+
+  attribute :company_name do |resource|
+    company = Company.find_by(id: resource.company_id)
+    company&.name
+  end
 end
