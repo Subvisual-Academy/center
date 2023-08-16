ba2b1d000817347e3d3ff454c75d141411d9463b
MFJess <jessica2001mf@gmail.com>
2023-08-16 13:17:14 +0100

    Add profile pic to user serializer.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,6 +4,9 @@ class UserSerializer
 
   attributes :id, :name, :email, :role, :base_office, :discord
 
+  attribute :profile_pic do |resource|
+    resource.profile_pic.url
+    
   attribute :company_name do |resource|
     company = Company.find_by(id: resource.company_id)
     company&.name
