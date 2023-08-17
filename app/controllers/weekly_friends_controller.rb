98be9401d04b40e661794bba73f061668027e870
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2023-08-17 11:28:12 +0100

    Serialize friend for FE

--- app/controllers/weekly_friends_controller.rb
+++ app/controllers/weekly_friends_controller.rb
@@ -4,10 +4,10 @@ class WeeklyFriendsController < ApplicationController
     active_activities = WeeklyActivity.active
 
     if active_activities.where(user_1: user).exists?
-      render json: active_activities.find_by(user_1: user).user_2
+      render json: UserSerializer.new(active_activities.find_by(user_1: user).user_2).serialize
 
     elsif active_activities.where(user_2: user).exists?
-      render json: active_activities.find_by(user_2: user).user_1
+      render json: UserSerializer.new(active_activities.find_by(user_2: user).user_1).serialize
 
     else
       render json: {error: "not_found"}, status: 404
