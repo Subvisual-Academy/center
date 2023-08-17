05e60b81d24e1612022e12c069e5fd05d68efc8d
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2023-08-17 17:27:49 +0100

    where doesnt work

--- db/migrate/20230817162236_add_default_pic_to_existing_users.rb
+++ db/migrate/20230817162236_add_default_pic_to_existing_users.rb
@@ -1,6 +1,8 @@
 class AddDefaultPicToExistingUsers < ActiveRecord::Migration[7.0]
   def change
-    User.where(profile_pic: nil).each do |user|
+    User.find_each do |user|
+      next if user.profile_pic.attached?
+
       user.profile_pic.attach(io: File.open("#{Rails.root}/app/assets/images/profile.png"), filename: "profile.png", content_type: "image/png")
     end
   end
